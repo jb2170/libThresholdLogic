cce6b9945978a1eea6c7f53d4dc71ba0e1c2ec9e
jb2170 <email@jb2170.com>
2024-02-14 22:50:05 +0000

    Merge adders into one file

--- src/libThresholdLogic/ExampleNetworks/__init__.py
+++ src/libThresholdLogic/ExampleNetworks/__init__.py
@@ -1,5 +1,3 @@
-from .FullAdder import FullAdder
-from .GenericBitAdder import GenericBitAdder
-from .HalfAdder import HalfAdder
+from .Adders import HalfAdder, FullAdder, GenericBitAdder
 from .LogicGates import HammingGate, GAND, GNAND, AND, NOR, NAND, OR, NOT, XOR, XNOR
 from .util import int_to_bit_tuple_lb, int_to_bit_tuple_bb, bit_tuple_lb_to_int, bit_tuple_bb_to_int
