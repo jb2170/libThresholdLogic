1439f32272d5ea3829278fbb3a14850e61798b26
jb2170 <email@jb2170.com>
2024-02-17 05:10:26 +0000

    Update Adders; simplify connection of neurons, the less manual indexing the better, as full explicit derivations will be given in README.md

--- src/libThresholdLogic/NeuronNetwork.py
+++ src/libThresholdLogic/NeuronNetwork.py
@@ -38,7 +38,7 @@ class NeuronNetwork:
             neuron_input.source = ConstNeuron(input_value)
 
         # this is a depth-first evaluation; having cache really helps
-        float_outputs = tuple((neuron(cache) for neuron in self.output_layer))
+        float_outputs = tuple(neuron(cache) for neuron in self.output_layer)
         valid_float_outputs = {0.0, 1.0}
 
         # lest a non-integer float arises eg from a non-perceptron neuron for some reason
@@ -47,3 +47,32 @@ class NeuronNetwork:
         int_outputs = tuple(int(o) for o in float_outputs)
 
         return int_outputs
+
+    def connect_inputs(self, *src: BaseNeuron) -> None:
+        """
+        Try connecting each neuron in `src` to the next available `ProxyNeuron` in `input_layer`
+        This is most useful when a `NeuronNetwork`'s behaviour is independent of
+        the ordering of the neurons in `input_layer`
+        For example in the input layer of `ExampleNetworks.Adders.GenericBitAdder` all
+        proxy neurons are connected to the perceptrons with the same weight
+        Hence we can indiscriminately use `connect_inputs`
+        """
+        neurons_of_interest = (n for n in self.input_layer if n.source is None)
+
+        for neuron_src in src:
+            try:
+                neuron_dest = next(neurons_of_interest)
+            except StopIteration as e:
+                raise IndexError("All neurons in destination list already connected") from e
+
+            neuron_dest.source = neuron_src
+
+    def pad_unconnected_inputs(self, value: float = 0.0) -> None:
+        """
+        Tie all unconnected inputs to a constant value
+        Useful for example in the first adder of a
+        ExampleNetworks.Adders.FullAdder ripple carry
+        """
+        for neuron in self.input_layer:
+            if neuron.source is None:
+                neuron.source = ConstNeuron(value)
